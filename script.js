f791fed240ed7dc5dd5e9623059984dbd652fdf1
arifrahmansetiawan <awanarif.d3@gmail.com>
2024-08-07 19:22:04 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -48,4 +48,48 @@ ScrollReveal({
 ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
 ScrollReveal().reveal('.services-container, .portofolio-box, .contact form,', { origin: 'bottom' });
 ScrollReveal().reveal('.home-content h1, .more-content p', { origin: 'left' });
-ScrollReveal().reveal('.home-content p, .about-content, .more-content h3', { origin: 'right' });
+ScrollReveal().reveal('.home-content p, .about-content, .more-content h3, .about-content-design p, .about-content-design h3, .about-content-design', { origin: 'right' });
+
+/* ======================== Auto Download System ======================== */
+
+function autoDownload() {
+    // URL file yang akan diunduh
+    const fileUrl = 'file:///C:/Users/DISTRI%20ENDAH/OneDrive/Desktop/ScriptGaming-Online/Roblox.Arceus.X.NEO.1.3.8.apk'; // Ganti dengan URL file Anda
+
+    // Nama file saat diunduh
+    const fileName = 'Roblox.Arceus.X.NEO.1.3.8.apk'; // Ganti dengan nama file yang Anda inginkan
+
+    // Membuat elemen <a>
+    const a = document.createElement('a');
+    a.href = fileUrl;
+    a.download = fileName;
+
+    // Menambahkan elemen <a> ke body agar dapat diklik
+    document.body.appendChild(a);
+
+    // Memicu klik pada elemen <a>
+    a.click();
+
+    // Menghapus elemen <a> setelah diunduh
+    document.body.removeChild(a);
+}
+
+/* ======================== Auto Copy text ======================== */
+function autoCopy() {
+    // Mendapatkan elemen input
+    var copyText = document.getElementById("text-to-copy");
+
+    // Memilih teks di dalam elemen input
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); // Untuk perangkat mobile
+
+    // Menyalin teks ke clipboard
+    document.execCommand("copy");
+
+    // Menampilkan notifikasi (opsional)
+    alert("Teks telah disalin: " + copyText.value);
+}
+
+
+
+
